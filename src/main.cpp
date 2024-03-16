89f0ecaf67147e0ace4c98c6310537fad59bb47a
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-03-16 20:30:40 +0200

    treewide: Unify formatting
    Use clang-format on all c++ files.

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,25 @@
 #include <QtQuick/QQuickView>
 #include <QTranslator>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-    QGuiApplication app(argc, argv);
-    QString locale = QLocale::system().name();
-    QTranslator translator;
-    translator.load(QString(":/i18n/cutie-settings_") + locale);
-    app.installTranslator(&translator);
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(
-        &engine, &QQmlApplicationEngine::objectCreated, &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
-    engine.load(url);
-    return app.exec();
+	QGuiApplication app(argc, argv);
+	QString locale = QLocale::system().name();
+	QTranslator translator;
+	translator.load(QString(":/i18n/cutie-settings_") + locale);
+	app.installTranslator(&translator);
+	QQmlApplicationEngine engine;
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(
+		&engine, &QQmlApplicationEngine::objectCreated, &app,
+		[url](QObject *obj, const QUrl &objUrl) {
+			if (!obj && url == objUrl)
+				QCoreApplication::exit(-1);
+		},
+		Qt::QueuedConnection);
+	engine.load(url);
+	return app.exec();
 }
